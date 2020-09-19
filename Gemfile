ea0c226d3d3273887efd4de92e0c5a7d7f522a01
Charlie Hsieh <charlieofficial@gmail.com>
2020-09-19 10:09:20 +0800

    add gem devise into Gemfile with view configs

--- Gemfile
+++ Gemfile
@@ -61,3 +61,6 @@ gem 'bullet', group: 'development'
 # For select timezone
 gem 'simple_form'
 
+# For Devise - User
+gem 'devise'
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :configs
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "books#index"
 end
